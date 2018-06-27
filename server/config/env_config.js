a87ffe43d1c7cf5a53c77efc79c375c08c96818c
Shrekie <thomaslindauer@mail.com>
2018-06-27 23:01:04 +0200

    whitespace

--- server/config/env_config.js
+++ server/config/env_config.js
@@ -3,8 +3,8 @@ var env = process.env.NODE_ENV || 'development';
 /*
 if(env == 'development'){
     const envar = require('./envar.js');
-	process.env.clientID = envar.clientID;
-	process.env.clientSecret = envar.clientSecret;
+    process.env.clientID = envar.clientID;
+    process.env.clientSecret = envar.clientSecret;
     process.env.sessionSecret = envar.sessionSecret;
 }
 */
